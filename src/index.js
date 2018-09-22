4d8a95da49d44ac6b10ff5ce052a0a85f32805c0
Nathan Williams <nlw2sx@virginia.edu>
2018-09-21 20:52:43 -0700

    Sync to git

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './scenes/App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
